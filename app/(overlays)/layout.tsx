a01160008fee09b6e7e3960b9cb3cc445eeb0e96
poppang <poppangbusiness@gmail.com>
2026-05-21 17:43:28 +0700

    init routes

---
+++ app/(overlays)/layout.tsx
@@ -0,0 +1,11 @@
+export default function OverlayLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
